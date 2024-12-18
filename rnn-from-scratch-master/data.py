469b34924ad02ba455bc7f61bea0abe9ed78d58c
NguyenThienBao <109329293+Limdim1604@users.noreply.github.com>
2024-12-18 13:59:51 +0700

    Add files via upload

---
+++ rnn-from-scratch-master/data.py
@@ -0,0 +1,16 @@
+train_sentences = [
+    "I wanna go to store",
+    "I am UITer",
+    "I am student",
+    "I go to school",
+    "I am gay",
+    "I love you",
+    "I love CS115",
+    "Nguyen Thien Bao",
+    "Dong Quoc Thang",
+    "Tran Le Minh Nhat",
+    "My name is Bao",
+    "My name is Thang",
+    "My name is Nhat",
+    "I enjoy learning AI",
+]
